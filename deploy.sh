0445870d3082cb19dbdb43f9e045fd3dba42e828
Erik Kopcha <erickopcha@gmail.com>
2021-03-14 23:29:46 +0200

    deploy

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:ErikKopcba/vue-vm-crs.git master:gh-pages
+git push -f git@github.com:erikkopcha/vue-vm-crs.git master:gh-pages
 
 cd -
